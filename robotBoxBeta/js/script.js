e9ff6e2b0b8abf0a49ac15adc775931a2cc35313
Rodion Grishaev <rodion.grishaev@warecorp.com>
2015-07-10 16:38:43 +0300

    robotBox: collides function.

--- robotBoxBeta/js/script.js
+++ robotBoxBeta/js/script.js
@@ -133,6 +133,18 @@
 		canvas.width = canvas.width;
 	}
 
+	function collides(x, y, r, b, x2, y2, r2, b2) {
+		return !(r <= x2 || x > r2 ||
+					 b <= y2 || y > b2);
+	}
+
+	function boxCollides(pos, size, pos2, size2) {
+		return collides(pos[0]-size[0]/2, pos[1]-size[1]/2,
+						pos[0]+size[0]/2, pos[1]+size[1]/2,
+						pos2[0]-size2[0]/2, pos2[1]-size2[1]/2,
+						pos2[0]+size2[0]/2, pos2[1]+size2[1]/2);
+	}
+
 	function moveWorldF(dt) {
 		switch(moveWorld){
 			case 0:

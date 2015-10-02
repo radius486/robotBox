f0a2a56571132fd434038b97915445b5aa65f8fe
Rodion Grishaev <rodion.grishaev@warecorp.com>
2015-10-02 20:01:40 +0300

    robotBox: New sprite for enemies.

--- robotBoxBeta/js/entities.js
+++ robotBoxBeta/js/entities.js
@@ -1,49 +1,49 @@
 (function(){
 
+  // Entities constructor
+  function Entities (pos, sprite, energy, speed, active){
+    this.pos = pos;
+    this.lastPosition = pos;
+    this.sprite = sprite;
+    this.energy = energy;
+    this.speed = speed;
+    this.active = false;
+    this.course = random(0,7);
+    this.cicle = 0;
+    this.endCicle = random(50,100);
+    this.angle = this.chooseAngle();
+  }
+
+  Entities.prototype.chooseAngle = function () {
+    switch(this.course){
+      case 0:
+        return Math.PI*3/2;
+        break;
+      case 1:
+        return Math.PI*11/6;
+        break;
+      case 2:
+        return Math.PI*2;
+        break;
+      case 3:
+        return Math.PI/4;
+        break;
+      case 4:
+        return Math.PI/2;
+        break;
+      case 5:
+        return Math.PI*3/4;
+        break;
+      case 6:
+        return Math.PI;
+        break;
+      case 7:
+        return Math.PI*4/3;
+        break;
+    }
+  }
+
   function createEntities(coords, array, sprite, energy, speed) {
-
-    // Entities constructor
-    function Entities (pos, sprite, energy, speed, active){
-      this.pos = pos;
-      this.lastPosition = pos;
-      this.sprite = sprite;
-      this.energy = energy;
-      this.speed = speed;
-      this.active = false;
-      this.course = random(0,7);
-      this.cicle = 0;
-      this.endCicle = random(50,100);
-      this.angle = this.chooseAngle();
-    }
-
-    Entities.prototype.chooseAngle = function () {
-      switch(this.course){
-        case 0:
-          return Math.PI*3/2;
-          break;
-        case 1:
-          return Math.PI*11/6;
-          break;
-        case 2:
-          return Math.PI*2;
-          break;
-        case 3:
-          return Math.PI/4;
-          break;
-        case 4:
-          return Math.PI/2;
-          break;
-        case 5:
-          return Math.PI*3/4;
-          break;
-        case 6:
-          return Math.PI;
-          break;
-        case 7:
-          return Math.PI*4/3;
-          break;
-      }
-    }
 
     for(var i=0;i<coords.length;i++){
       var entity = new Entities ([coords[i][0],coords[i][1]], sprite, energy, speed);
